edbd06b5bdd467e529d2de2e4a92997c449518e2
Sudhir <sudheer.shirsath@gmail.com>
2017-05-25 00:26:59 +0530

    finished the 2.1_tcp_client

--- chapter2/2.1_tcp_client.py
+++ chapter2/2.1_tcp_client.py
@@ -1,7 +1,23 @@
+#!/usr/bin/python
+
 import socket
 
 target_host = raw_input("Enter the host: ")
-target_port = raw_input("Enter the port number: ")
+target_port = int(raw_input("Enter the port number: "))
 
-# create a socket object
-client = socket.socket
+# this information will be sent to the server
+host = raw_input("Enter the header --> Host: ")
+
+# create a socket object --> socket.socket(family, type)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client to the target
+client.connect((target_host,target_port))
+
+# send some data
+client.send("GET / HTTP/1.1\r\nHost: "+host+"\r\n\r\n")
+
+# receive some data client.recv(buffersize)
+response = client.recv(4096)
+
+print response
